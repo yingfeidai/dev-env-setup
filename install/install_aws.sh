27b586469b6918ac56ae3154aa1f67ac6011bcec
dyf96 <yingfei.dai96@gmail.com>
2024-10-09 21:55:54 +0900

    feat: ✨ add sh

---
+++ install/install_aws.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ---- Step 8: Install AWS CLI ----
+# Installs the AWS CLI for managing AWS services
+echo "Installing AWS CLI..."
+brew install awscli
---
+++ install/install_xcode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ---- Step 1: Install Xcode Command Line Tools ----
+# Installs Apple's Xcode command line tools, a foundational step for development on macOS
+echo "Installing Xcode Command Line Tools..."
+xcode-select --install
